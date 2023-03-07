c77900b2f469ed9ebd5d121c29ac9c10369ab1ff
nosuz <nosuzuki@postcard.st>
2023-03-07 10:47:07 +0900

    Implemented embedded-hal for I2C.

--- samples/i2c/src/main.rs
+++ samples/i2c/src/main.rs
@@ -15,17 +15,22 @@ use ch32v103_hal::i2c::*;
 use ch32v103_hal::delay::*;
 
 const ADT7410_ADDR: u8 = 0x48;
+const RESET_ADT7410: [u8; 1] = [0x2f];
+const ONE_SHOT: [u8; 2] = [0x03, 0x20];
+const READ_TEMP: [u8; 1] = [0x00];
+const READ_ID: [u8; 1] = [0x08];
+const READ_STATUS: [u8; 1] = [0x02];
 
 #[entry]
 fn main() -> ! {
     let peripherals = ch32v103::Peripherals::take().unwrap();
     let rcc = peripherals.RCC.constrain();
 
-    // let clocks = rcc.cfgr.freeze();
-    let clocks = rcc.cfgr
-        .use_pll((48).mhz(), PllClkSrc::UseHsi)
-        .hclk_prescale(HclkPreScale::Div4)
-        .freeze();
+    let clocks = rcc.cfgr.freeze();
+    // let clocks = rcc.cfgr
+    //     .use_pll((48).mhz(), PllClkSrc::UseHsi)
+    //     .hclk_prescale(HclkPreScale::Div4)
+    //     .freeze();
 
     let gpioa = peripherals.GPIOA.split();
     let pa9 = gpioa.pa9.into_multiplex_push_pull_output();
@@ -47,73 +52,55 @@ fn main() -> ! {
     let pb9 = gpiob.pb9.into_multiplex_open_drain_output();
 
     // required peripherals.I2C1 to occupy
-    let i2c = I2c::i2c1(peripherals.I2C1, (pb6, pb7), &clocks);
+    let mut i2c = I2c::i2c1(peripherals.I2C1, (pb6, pb7), &clocks, I2cMode::Fast);
 
     let mut delay = Delay::new(&clocks);
-    let mut count = 0;
 
     led1.set_high().unwrap();
     led2.set_high().unwrap();
 
     // Reset ADT7310
-    // i2c.start();
-    // i2c.addr(ADT7410_ADDR, false);
-    // i2c.send(0x2f);
-    // i2c.stop();
-    // delay.delay_ms(1);
+    i2c.write(ADT7410_ADDR, &RESET_ADT7410).unwrap();
+    // wait loading default config
+    delay.delay_us(300);
+
+    // finishing the first temp measurement
+    loop {
+        delay.delay_ms(10);
+
+        let mut stat: [u8; 1] = [0x00];
+        i2c.write_read(ADT7410_ADDR, &READ_STATUS, &mut stat).unwrap();
+        if (stat[0] & 0x80) == 0 {
+            break;
+        }
+    }
 
     loop {
         led1.set_low().unwrap();
         led2.set_low().unwrap();
 
-        // serial write with format sample.
-        writeln!(&mut log, "Hello {}: {}", "world", count).unwrap();
+        let mut id: [u8; 1] = [0x00];
+        i2c.write_read(ADT7410_ADDR, &READ_ID, &mut id).unwrap();
+        writeln!(&mut log, "ID:{:02X}", id[0]).unwrap();
 
-        i2c.start();
-        led1.set_high().unwrap();
-        i2c.addr(ADT7410_ADDR, false);
-        loop {
-            writeln!(&mut log, "{:04X}", i2c.status()).unwrap();
-        }
-        led2.set_high().unwrap();
-        i2c.send(0x08);
-        i2c.start();
-        i2c.addr(ADT7410_ADDR, true);
-        // i2c.send(0x08);
-        writeln!(&mut log, "{:02X}", i2c.read(false)).unwrap();
-        i2c.stop();
+        delay.delay_ms(1);
 
-        delay.delay_ms(5);
-
-        // One-shot measure
-        i2c.start();
-        i2c.addr(ADT7410_ADDR, false);
-        i2c.send(0x03);
-        i2c.send(0x20);
-        i2c.stop();
-
+        i2c.write(ADT7410_ADDR, &ONE_SHOT).unwrap();
         delay.delay_ms(240);
 
-        i2c.start();
-        i2c.addr(ADT7410_ADDR, false);
-        i2c.send(0x0);
-        i2c.start();
-        i2c.addr(ADT7410_ADDR, true);
-        let temp_h = i2c.read(true);
-        let temp_l = i2c.read(false);
-        i2c.stop();
+        let mut buffer: [u8; 2] = [0x00; 2]; // init by 0x00
+        i2c.write_read(ADT7410_ADDR, &READ_TEMP, &mut buffer).unwrap();
+        let _temp = ((buffer[0] as u16) << 8) | (buffer[1] as u16);
 
-        let _temp = ((temp_h as u16) << 8) | (temp_l as u16);
         let temp = if (_temp & 0x8000) == 0 {
             ((_temp >> 3) as f32) / 16.0
         } else {
             (((_temp >> 3) as f32) - 8191.0) / 16.0
         };
-        writeln!(&mut log, "{:+.1}", temp).unwrap();
+        writeln!(&mut log, "Temp:{:+.1}", temp).unwrap();
 
         led1.set_high().unwrap();
         led2.set_high().unwrap();
         delay.delay_ms(200);
-        count += 1;
     }
 }
--- src/i2c.rs
+++ src/i2c.rs
@@ -1,8 +1,32 @@
+use embedded_hal::blocking::i2c;
+
 use ch32v1::ch32v103::{ RCC, I2C1 };
 // use crate::time::*;
 use crate::rcc::*;
 use crate::gpio::*;
 use crate::gpio::gpiob::{ PB6, PB7 };
+
+// define I2C error
+#[derive(Debug)]
+pub enum I2cError {
+    // Bus Error (BERR)
+    BusError,
+    // Acknowledge Failure (AF)
+    Nak,
+    // Arbitration Lost (ARLO)
+    ArbitrationError,
+    // Overrun/ Underrun Error (OVR)
+    OverrunError,
+    // Crc check error
+    CrcError,
+    // Not specify error reason
+    UnknownError,
+}
+
+pub enum I2cMode {
+    Standard,
+    Fast,
+}
 
 pub unsafe trait SclPin<I2C> {
     fn remap(&self) -> bool;
@@ -23,22 +47,67 @@ unsafe impl SdaPin<I2C1> for PB7<AltOutput<OpenDrain>> {
 }
 
 // Serial abstraction
-pub struct I2c<PINS> {
-    i2c: I2C1,
+pub struct I2c<I2C, PINS> {
+    i2c: I2C,
     pins: PINS,
 }
 
-impl<SC, SD> I2c<(SC, SD)> {
+impl<SC, SD> I2c<I2C1, (SC, SD)> {
     // init I2C1
-    pub fn i2c1(i2c: I2C1, pins: (SC, SD), clocks: &Clocks) -> Self
+    // TODO: I2c::new() for I2C1 and I2C2
+    pub fn i2c1(i2c: I2C1, pins: (SC, SD), clocks: &Clocks, mode: I2cMode) -> Self
         where SC: SclPin<I2C1>, SD: SdaPin<I2C1>
     {
         unsafe {
             (*RCC::ptr()).apb1pcenr.modify(|_, w| w.i2c1en().set_bit());
-            // (*I2C1::ptr()).ctlr2.modify(|_, w| w.freq().bits(0b1100));
+            let base_freq = clocks.pclk1().0 / 1000_000; // by MHz
+            assert!((base_freq >= 2) & (base_freq <= 36));
+            (*I2C1::ptr()).ctlr2.modify(|_, w| w.freq().bits(base_freq as u8));
 
-            // For testing
-            (*I2C1::ptr()).ckcfgr.modify(|_, w| w.f_s().set_bit().ccr().bits(40)); // fast mode
+            match mode {
+                // CCR values are referred to STM32F4xx (M0090 Rev 19) datasheet
+                I2cMode::Standard => {
+                    // Thigh = CCR * TPCLK1
+                    // Tlow = CCR * TPCLK1
+                    // (1 / 100kHz) * (1 / 2) = CCR * (1 / PCLK1)
+                    let ccr = 5 * base_freq;
+                    (*I2C1::ptr()).ckcfgr.modify(|_, w|
+                        w
+                            .f_s()
+                            .clear_bit()
+                            .ccr()
+                            .bits(ccr as u16)
+                    );
+                }
+                I2cMode::Fast => {
+                    let ccr = if base_freq < 10 {
+                        (*I2C1::ptr()).ckcfgr.modify(|_, w| w.duty().clear_bit());
+
+                        // If DUTY = 0:
+                        // Thigh = CCR * TPCLK1
+                        // Tlow = 2 * CCR * TPCLK1
+                        // (1 / 400kHz) * (1 / 3) = CCR * (1 / PCLK1)
+                        (base_freq * 10) / 12
+                    } else {
+                        // If DUTY = 1:
+                        (*I2C1::ptr()).ckcfgr.modify(|_, w| w.duty().set_bit());
+
+                        // Thigh = 9 * CCR * TPCLK1
+                        // Tlow = 16 * CCR * TPCLK1
+                        // (1 / 400kHz) * (9 /25) = 9 * CCR * (1 / PCLK1)
+                        // base_freq must be >10HHz
+                        base_freq / 10
+                    };
+                    (*I2C1::ptr()).ckcfgr.modify(|_, w|
+                        w
+                            .f_s()
+                            .set_bit()
+                            .ccr()
+                            .bits(ccr as u16)
+                    );
+                }
+            }
+
             // Enable I2C
             (*I2C1::ptr()).ctlr1.modify(|_, w| w.pe().set_bit());
         }
@@ -48,56 +117,280 @@ impl<SC, SD> I2c<(SC, SD)> {
         }
     }
 
-    pub fn start(&self) {
-        unsafe {
-            // START condition
-            (*I2C1::ptr()).ctlr1.modify(|_, w| w.start().set_bit());
-            // wait start transmitted
-            while (*I2C1::ptr()).star1.read().sb().bit_is_clear() {}
-        }
-    }
-
-    pub fn stop(&self) {
-        unsafe {
-            // All transmission completed
-            while (*I2C1::ptr()).star1.read().btf().bit_is_clear() {}
-            // STOP condition
-            (*I2C1::ptr()).ctlr1.modify(|_, w| w.stop().set_bit());
-        }
-    }
-
-    pub fn addr(&self, addr: u8, rw: bool) {
-        unsafe {
-            (*I2C1::ptr()).datar.write(|w|
-                w.bits(0x400_u16 | ((addr as u16) << 1) | (if rw { 1 } else { 0 }))
-            );
-            // Wait transmitted ADDR and RW bit.
-            // while (*I2C1::ptr()).star1.read().addr().bit_is_clear() {}
-            // (*I2C1::ptr()).star2.read();
-        }
-    }
-
-    pub fn send(&self, data: u8) {
-        unsafe {
-            while (*I2C1::ptr()).star1.read().tx_e().bit_is_clear() {}
-            (*I2C1::ptr()).datar.write(|w| w.bits(data as u16));
-        }
-    }
-
-    pub fn read(&self, ack: bool) -> u8 {
-        unsafe {
-            if ack {
-                (*I2C1::ptr()).ctlr1.modify(|_, w| w.ack().set_bit());
-            } else {
-                (*I2C1::ptr()).ctlr1.modify(|_, w| w.ack().clear_bit());
-            }
-            // Wait ready to read
-            while (*I2C1::ptr()).star1.read().rx_ne().bit_is_clear() {}
-            (*I2C1::ptr()).datar.read().bits() as u8
-        }
-    }
-
     pub fn status(&self) -> u16 {
         unsafe { (*I2C1::ptr()).star1.read().bits() as u16 }
+    }
+}
+
+macro_rules! busy_wait {
+    ($flag:ident) => {
+        loop {
+            let stat = (*I2C1::ptr()).star1.read();
+            if stat.$flag().bit_is_set() {
+                break;
+            } else if stat.bits() & 0x1f00 > 0 {
+                // Make STOP condition before exit
+                (*I2C1::ptr()).ctlr1.modify(|_, w| w.stop().set_bit());
+                return Err(
+                    if stat.pecerr().bit_is_set() {
+                        I2cError::CrcError
+                    } else if stat.ovr().bit_is_set() {
+                        I2cError::OverrunError
+                    } else if stat.af().bit_is_set() {
+                        I2cError::Nak
+                    } else if stat.arlo().bit_is_set() {
+                        I2cError::ArbitrationError
+                    } else if stat.berr().bit_is_set() {
+                        I2cError::BusError
+                    } else {
+                        I2cError::UnknownError
+                    }
+                );
+            }
+        }
+    };
+}
+
+impl<I2C, PINS> i2c::Write<u8> for I2c<I2C, PINS> {
+    type Error = I2cError;
+
+    fn write(&mut self, address: u8, bytes: &[u8]) -> Result<(), Self::Error> {
+        unsafe {
+            // Reset status register
+            (*I2C1::ptr()).star1.write(|w| w.bits(0));
+
+            // make START condition
+            (*I2C1::ptr()).ctlr1.modify(|_, w| w.start().set_bit());
+            // wait start transmitted
+            busy_wait!(sb);
+
+            // send ADDRESS with WRITE flag(0)
+            (*I2C1::ptr()).datar.write(|w| w.bits((address as u16) << 1));
+            // Wait transmitted ADDR and RW bit.
+            busy_wait!(addr);
+            (*I2C1::ptr()).star2.read();
+
+            // send all data
+            for byte in bytes {
+                busy_wait!(tx_e);
+                (*I2C1::ptr()).datar.write(|w| w.bits(*byte as u16));
+            }
+
+            // Wait WRITE complete
+            busy_wait!(btf);
+            // make STOP condition
+            (*I2C1::ptr()).ctlr1.modify(|_, w| w.stop().set_bit());
+        }
+
+        Ok(())
+    }
+}
+
+impl<I2C, PINS> i2c::WriteIter<u8> for I2c<I2C, PINS> {
+    type Error = I2cError;
+
+    fn write<B>(&mut self, address: u8, bytes: B) -> Result<(), Self::Error>
+        where B: IntoIterator<Item = u8>
+    {
+        unsafe {
+            // Reset status register
+            (*I2C1::ptr()).star1.write(|w| w.bits(0));
+
+            // make START condition
+            (*I2C1::ptr()).ctlr1.modify(|_, w| w.start().set_bit());
+            // wait start transmitted
+            busy_wait!(sb);
+
+            // send ADDRESS with WRITE flag(0)
+            (*I2C1::ptr()).datar.write(|w| w.bits((address as u16) << 1));
+            // Wait transmitted ADDR and RW bit.
+            busy_wait!(addr);
+            (*I2C1::ptr()).star2.read();
+
+            // send all data
+            for byte in bytes {
+                busy_wait!(tx_e);
+                (*I2C1::ptr()).datar.write(|w| w.bits(byte as u16));
+            }
+
+            // Wait WRITE complete
+            busy_wait!(btf);
+            // make STOP condition
+            (*I2C1::ptr()).ctlr1.modify(|_, w| w.stop().set_bit());
+        }
+
+        Ok(())
+    }
+}
+
+impl<I2C, PINS> i2c::Read<u8> for I2c<I2C, PINS> {
+    type Error = I2cError;
+
+    fn read(&mut self, address: u8, buffer: &mut [u8]) -> Result<(), Self::Error> {
+        unsafe {
+            // Reset status register
+            (*I2C1::ptr()).star1.write(|w| w.bits(0));
+
+            // make START condition
+            (*I2C1::ptr()).ctlr1.modify(|_, w| w.start().set_bit());
+            // wait start transmitted
+            busy_wait!(sb);
+
+            // send ADDRESS with WRITE flag
+            (*I2C1::ptr()).datar.write(|w| w.bits(((address as u16) << 1) | 0b1));
+            // Wait transmitted ADDR and RW bit.
+            busy_wait!(addr);
+            (*I2C1::ptr()).star2.read();
+
+            // read all data
+            (*I2C1::ptr()).ctlr1.modify(|_, w| w.ack().set_bit());
+            let buffer_last = buffer.len() - 1;
+            for (i, byte) in buffer.iter_mut().enumerate() {
+                if i == buffer_last {
+                    // Return NACK
+                    (*I2C1::ptr()).ctlr1.modify(|_, w| w.ack().clear_bit());
+                }
+                // Wait ready to read
+                busy_wait!(rx_ne);
+                *byte = (*I2C1::ptr()).datar.read().bits() as u8;
+            }
+
+            // make STOP condition
+            (*I2C1::ptr()).ctlr1.modify(|_, w| w.stop().set_bit());
+        }
+
+        Ok(())
+    }
+}
+
+impl<I2C, PINS> i2c::WriteRead<u8> for I2c<I2C, PINS> {
+    type Error = I2cError;
+
+    fn write_read(
+        &mut self,
+        address: u8,
+        bytes: &[u8],
+        buffer: &mut [u8]
+    ) -> Result<(), Self::Error> {
+        unsafe {
+            // Reset status register
+            (*I2C1::ptr()).star1.write(|w| w.bits(0));
+
+            // make START condition
+            (*I2C1::ptr()).ctlr1.modify(|_, w| w.start().set_bit());
+            // wait start transmitted
+            busy_wait!(sb);
+
+            // send ADDRESS with WRITE flag(0)
+            (*I2C1::ptr()).datar.write(|w| w.bits((address as u16) << 1));
+            // Wait transmitted ADDR and RW bit.
+            busy_wait!(addr);
+            (*I2C1::ptr()).star2.read();
+
+            // send all data
+            for byte in bytes {
+                busy_wait!(tx_e);
+                (*I2C1::ptr()).datar.write(|w| w.bits(*byte as u16));
+            }
+
+            // Wait WRITE complete
+            busy_wait!(tx_e);
+
+            // make START again
+            (*I2C1::ptr()).ctlr1.modify(|_, w| w.start().set_bit());
+            // wait start transmitted
+            busy_wait!(sb);
+
+            // send ADDRESS with WRITE flag
+            (*I2C1::ptr()).datar.write(|w| w.bits(((address as u16) << 1) | 0b1));
+            // Wait transmitted ADDR and RW bit.
+            busy_wait!(addr);
+            (*I2C1::ptr()).star2.read();
+
+            // read all data
+            (*I2C1::ptr()).ctlr1.modify(|_, w| w.ack().set_bit());
+            let buffer_last = buffer.len() - 1;
+            for (i, byte) in buffer.iter_mut().enumerate() {
+                if i == buffer_last {
+                    // Return NACK
+                    (*I2C1::ptr()).ctlr1.modify(|_, w| w.ack().clear_bit());
+                }
+                // Wait ready to read
+                busy_wait!(rx_ne);
+                *byte = (*I2C1::ptr()).datar.read().bits() as u8;
+            }
+
+            // make STOP condition
+            (*I2C1::ptr()).ctlr1.modify(|_, w| w.stop().set_bit());
+        }
+
+        Ok(())
+    }
+}
+
+impl<I2C, PINS> i2c::WriteIterRead<u8> for I2c<I2C, PINS> {
+    type Error = I2cError;
+
+    fn write_iter_read<B>(
+        &mut self,
+        address: u8,
+        bytes: B,
+        buffer: &mut [u8]
+    ) -> Result<(), Self::Error>
+        where B: IntoIterator<Item = u8>
+    {
+        unsafe {
+            // Reset status register
+            (*I2C1::ptr()).star1.write(|w| w.bits(0));
+
+            // make START condition
+            (*I2C1::ptr()).ctlr1.modify(|_, w| w.start().set_bit());
+            // wait start transmitted
+            busy_wait!(sb);
+
+            // send ADDRESS with WRITE flag(0)
+            (*I2C1::ptr()).datar.write(|w| w.bits((address as u16) << 1));
+            // Wait transmitted ADDR and RW bit.
+            busy_wait!(addr);
+            (*I2C1::ptr()).star2.read();
+
+            // send all data
+            for byte in bytes {
+                busy_wait!(tx_e);
+                (*I2C1::ptr()).datar.write(|w| w.bits(byte as u16));
+            }
+
+            // Wait WRITE complete
+            busy_wait!(tx_e);
+
+            // make START again
+            (*I2C1::ptr()).ctlr1.modify(|_, w| w.start().set_bit());
+            // wait start transmitted
+            busy_wait!(sb);
+
+            // send ADDRESS with WRITE flag
+            (*I2C1::ptr()).datar.write(|w| w.bits(((address as u16) << 1) | 0b1));
+            // Wait transmitted ADDR and RW bit.
+            busy_wait!(addr);
+            (*I2C1::ptr()).star2.read();
+
+            // read all data
+            (*I2C1::ptr()).ctlr1.modify(|_, w| w.ack().set_bit());
+            let buffer_last = buffer.len() - 1;
+            for (i, byte) in buffer.iter_mut().enumerate() {
+                if i == buffer_last {
+                    // Return NACK
+                    (*I2C1::ptr()).ctlr1.modify(|_, w| w.ack().clear_bit());
+                }
+                // Wait ready to read
+                busy_wait!(rx_ne);
+                *byte = (*I2C1::ptr()).datar.read().bits() as u8;
+            }
+
+            // make STOP condition
+            (*I2C1::ptr()).ctlr1.modify(|_, w| w.stop().set_bit());
+        }
+
+        Ok(())
     }
 }
