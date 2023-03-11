70ab98f4dc7b5f506b43c4c5f43fea0513ffdcd8
nosuz <nosuzuki@postcard.st>
2023-03-11 15:27:25 +0900

    Fix e8d8d5958ae3930

--- src/adc.rs
+++ src/adc.rs
@@ -41,23 +41,35 @@ adc_channel!(PB1, 9);
 pub struct Adc<ADC> {
     adc: ADC,
     delay: Delay,
+    tconv: u32,
 }
 
 impl<ADCX> Adc<ADCX> {
     pub fn adc(adc: ADCX, clocks: &Clocks) -> Self {
+        let (adcpre_bits, adc_clock) = match clocks.pclk2().0 / 14_000_000 {
+            0..=1 => (0b00, clocks.pclk2().0 / 2),
+            2..=3 => (0b01, clocks.pclk2().0 / 4),
+            4..=5 => (0b10, clocks.pclk2().0 / 6),
+            _ => (0x11, clocks.pclk2().0 / 8),
+        };
+
         unsafe {
+            (*RCC::ptr()).cfgr0.modify(|_, w| w.adcpre().bits(adcpre_bits));
             (*RCC::ptr()).apb2pcenr.modify(|_, w| w.adcen().set_bit());
         }
 
+        let tconv = (240.0 + 12.5) / ((adc_clock / 1_000_000) as f32);
         Self {
             adc: adc,
             delay: Delay::new(clocks),
+            tconv: (tconv as u32) + 1, // in us
         }
     }
 
     fn power_up(&mut self) {
         unsafe {
             (*ADC::ptr()).ctlr2.modify(|_, w| w.adon().set_bit());
+            //wait Tstab
             self.delay.delay_us(1);
             (*ADC::ptr()).ctlr2.modify(|_, w| w.adon().set_bit());
         }
@@ -69,12 +81,14 @@ impl<ADCX> Adc<ADCX> {
         }
     }
 
-    fn do_conversion(&self, channel: u8) -> u16 {
+    fn do_conversion(&mut self, channel: u8) -> u16 {
         unsafe {
             (*ADC::ptr()).rsqr3.modify(|_, w| w.sq1().bits(channel));
             (*ADC::ptr()).rsqr1.modify(|_, w| w.l().bits(0x1));
 
+            // start convertion
             (*ADC::ptr()).ctlr2.modify(|_, w| w.adon().set_bit());
+            self.delay.delay_us(self.tconv);
 
             (*ADC::ptr()).rdatar.read().bits() as u16
         }
