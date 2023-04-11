a8ac17d1ba6da4a0cfac01b98fe4705d7f7ed6a6
nosuz <nosuzuki@postcard.st>
2023-04-11 11:43:24 +0900

    Separated blinky to blinking a single LED and multiple LEDs.
    But binary size of the single LED is 2 times larger than expected.

---
+++ samples/blink-leds/src/main.rs
@@ -0,0 +1,78 @@
+#![no_std]
+#![no_main]
+
+// provide implementation for critical-section
+use ch32v_rt::entry;
+use panic_halt as _;
+
+// use ch32v1::ch32v103; // PAC for CH32V103
+use ch32v1::ch32v103::Peripherals;
+use ch32v103_hal::prelude::*;
+use ch32v103_hal::rcc::*;
+use ch32v103_hal::gpio::*;
+use ch32v103_hal::delay::*;
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take().unwrap();
+    let rcc = peripherals.RCC.constrain();
+
+    // let clocks = rcc.cfgr.freeze();
+    // 72MHz not worked for me
+    // let clocks = rcc.cfgr.use_pll((64).mhz(), PllClkSrc::Hsi).freeze();
+    let clocks = rcc.cfgr.use_pll((48).mhz(), PllClkSrc::HsiDiv2).hclk((24).mhz()).freeze();
+
+    let gpioa = peripherals.GPIOA.split();
+    let mut led_r1 = gpioa.pa4.into_push_pull_output();
+    let mut led_r2 = gpioa.pa5.into_push_pull_output();
+
+    let gpiob = peripherals.GPIOB.split();
+    let mut led1 = gpiob.pb2.into_push_pull_output();
+    let mut led2 = gpiob.pb15.into_push_pull_output();
+    let mut led3 = gpiob.pb0.into_push_pull_output();
+
+    // Push-pull at max 50MHz
+    // unsafe {
+    //     gpiob.cfglr.modify(|_, w| w.cnf0().bits(0b00).mode0().bits(0b11))
+    // };
+
+    let mut delay = Delay::new(&clocks);
+    led2.set_high().unwrap();
+    loop {
+        led_r1.set_high().unwrap();
+        delay.delay_ms(100);
+        led_r1.set_low().unwrap();
+        delay.delay_ms(100);
+
+        // gpiob.outdr.modify(|_, w| w.odr0().set_bit());
+        led1.set_high().unwrap();
+        led3.set_state(PinState::Low).unwrap();
+
+        led_r1.set_high().unwrap();
+        led_r2.set_low().unwrap();
+
+        // toggle LED2
+        // if led2.is_set_high().unwrap() {
+        //     led2.set_low().unwrap();
+        // } else {
+        //     led2.set_high().unwrap();
+        // }
+        led2.toggle().unwrap();
+
+        delay.delay_ms(500);
+
+        // gpiob.outdr.modify(|_, w| w.odr0().clear_bit());
+        led1.set_low().unwrap();
+        led3.set_state(PinState::High).unwrap();
+
+        led_r1.set_low().unwrap();
+        led_r2.set_high().unwrap();
+
+        // toggle LED2
+        led2.toggle().unwrap();
+        delay.delay_ms(200);
+        led2.toggle().unwrap();
+
+        delay.delay_ms(500);
+    }
+}
--- samples/blinky/src/main.rs
+++ samples/blinky/src/main.rs
@@ -17,19 +17,13 @@ fn main() -> ! {
     let peripherals = Peripherals::take().unwrap();
     let rcc = peripherals.RCC.constrain();
 
-    // let clocks = rcc.cfgr.freeze();
+    let clocks = rcc.cfgr.freeze();
     // 72MHz not worked for me
     // let clocks = rcc.cfgr.use_pll((64).mhz(), PllClkSrc::Hsi).freeze();
-    let clocks = rcc.cfgr.use_pll((48).mhz(), PllClkSrc::HsiDiv2).hclk((24).mhz()).freeze();
-
-    let gpioa = peripherals.GPIOA.split();
-    let mut led_r1 = gpioa.pa4.into_push_pull_output();
-    let mut led_r2 = gpioa.pa5.into_push_pull_output();
+    // let clocks = rcc.cfgr.use_pll((48).mhz(), PllClkSrc::HsiDiv2).hclk((24).mhz()).freeze();
 
     let gpiob = peripherals.GPIOB.split();
     let mut led1 = gpiob.pb2.into_push_pull_output();
-    let mut led2 = gpiob.pb15.into_push_pull_output();
-    let mut led3 = gpiob.pb0.into_push_pull_output();
 
     // Push-pull at max 50MHz
     // unsafe {
@@ -37,42 +31,11 @@ fn main() -> ! {
     // };
 
     let mut delay = Delay::new(&clocks);
-    led2.set_high().unwrap();
+
     loop {
-        led_r1.set_high().unwrap();
-        delay.delay_ms(100);
-        led_r1.set_low().unwrap();
-        delay.delay_ms(100);
-
-        // gpiob.outdr.modify(|_, w| w.odr0().set_bit());
         led1.set_high().unwrap();
-        led3.set_state(PinState::Low).unwrap();
-
-        led_r1.set_high().unwrap();
-        led_r2.set_low().unwrap();
-
-        // toggle LED2
-        // if led2.is_set_high().unwrap() {
-        //     led2.set_low().unwrap();
-        // } else {
-        //     led2.set_high().unwrap();
-        // }
-        led2.toggle().unwrap();
-
         delay.delay_ms(500);
-
-        // gpiob.outdr.modify(|_, w| w.odr0().clear_bit());
         led1.set_low().unwrap();
-        led3.set_state(PinState::High).unwrap();
-
-        led_r1.set_low().unwrap();
-        led_r2.set_high().unwrap();
-
-        // toggle LED2
-        led2.toggle().unwrap();
-        delay.delay_ms(200);
-        led2.toggle().unwrap();
-
         delay.delay_ms(500);
     }
 }
